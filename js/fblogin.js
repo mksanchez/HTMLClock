74f199875e4d34b492ec4d499cddf6cf35b5972b
mariel.k.sanchez@gmail.com <mariel.k.sanchez@gmail.com>
2015-02-19 18:31:27 -0800

    checking userID

--- js/fblogin.js
+++ js/fblogin.js
@@ -8,7 +8,8 @@ function statusChangeCallback(response) {
   // for FB.getLoginStatus().
   if (response.status === 'connected') {
     console.log("check");
-     console.log(response[0].userID);
+    console.log(response[0].userID);
+    console.log("wut");
     // Logged into your app and Facebook.
     testAPI();
 
